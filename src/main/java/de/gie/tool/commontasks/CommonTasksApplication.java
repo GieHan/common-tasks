5ea1e79ff0899418d61f659c43290cdf873fda96
GieHan <Gie.Hanggoro@Hotmail.com>
2022-08-09 13:46:13 +0200

    initial commit

---
+++ src/main/java/de/gie/tool/commontasks/CommonTasksApplication.java
@@ -0,0 +1,13 @@
+package de.gie.tool.commontasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/gie/tool/commontasks/CommonTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gie.tool.commontasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
